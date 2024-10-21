fd2840a9387a24c6a97049919f03a24347753496
zzz-myam <jiyoo0315@naver.com>
2024-10-21 09:59:40 +0900

    fix: 코드 정리

--- practice2/todo.js
+++ practice2/todo.js
@@ -1,19 +1,19 @@
 const todolist = document.querySelector('.todo-list');
-
-//------
-
 const btn = document.querySelector('.add-button');
+
 const buttonClick = () => {
-  const span = document.createElement('span');
-  const dbtn = document.createElement('button');
   const li = document.createElement('li');
+  const span = document.createElement('span');
+  const deleteBtn = document.createElement('button');
+
   const inputvalue = document.querySelector('#todo').value;
   todolist.appendChild(li);
   li.appendChild(span);
-  li.appendChild(dbtn);
-  span.innerText = inputvalue;
-  dbtn.innerText = '삭제';
-  dbtn.addEventListener('click', () => {
+  li.appendChild(deleteBtn);
+
+  span.innerText = inputvalue; /*사용자가 입력한 내용을 span에 넣는 것*/
+  deleteBtn.innerText = '삭제';
+  deleteBtn.addEventListener('click', () => {
     todolist.removeChild(li);
   });
 };
