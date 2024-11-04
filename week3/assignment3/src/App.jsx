8a86b2eb54d3854ce4c2465629dcf5da95f8cbe2
zzz-myam <jiyoo0315@naver.com>
2024-11-04 17:44:33 +0900

    feat: 헤더 만드는 중

--- week3/assignment3/src/App.jsx
+++ week3/assignment3/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css';
 
 function App() {
-  return (
-    <>
-      
-    </>
-  );
+  return <>
+    <div className='header'>
+      <h1 className='title'>1 to 50</h1>
+      <button className='gameButton'>게임</button>
+      <button className='rankingButton'>랭킹</button>
+      <select className='levelSelect'>
+        <option value="Level1">Level1</option>
+        <option value="Level2">Level2</option>
+        <option value="Level3">Level3</option>
+      </select>
+    </div>
+  </>;
 }
 
 export default App;
